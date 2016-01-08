84761cc317976594efa51401fcdd05e82c700d3d
verlak <verlak46@gmail.com>
2016-01-08 19:54:28 +0100

    Removed ngMap injection

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('starter', ['ionic', 'ngCordova', 'ngStorage', 'pascalprecht.translate', 'uiGmapgoogle-maps', 'ngMap', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'ngCordova', 'ngStorage', 'pascalprecht.translate', 'uiGmapgoogle-maps', 'starter.controllers', 'starter.services'])
 
 .run(function ($ionicPlatform) {
 
